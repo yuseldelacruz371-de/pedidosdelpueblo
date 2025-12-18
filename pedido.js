a5705c91dba871ce3b9ff43314ba887d983c642b
yuseldelacruz371-de <yuseldelacruz371@gmail.com>
2025-12-17 20:22:50 -0600

    Update pedido.js

--- pedido.js
+++ pedido.js
@@ -1,4 +1,4 @@
-const servicioActivo = true; 
+const servicioActivo = false; 
 
 const options = document.getElementById("options");
 const cart = document.getElementById("cart");
@@ -127,6 +127,7 @@ ${items.map(i => "• " + i).join("\n")}
 
   window.open(`https://wa.me/+527811046148?text=${encodeURIComponent(message)}`);
 }
+
 
 
 
